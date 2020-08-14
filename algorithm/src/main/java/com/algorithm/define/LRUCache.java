d1757801a55358c7b891341232f4ab7035306a2c
290579856@qq.com <290579856@qq.com>
2020-08-14 10:31:36 +0800

    添加算法题目

--- algorithm/src/main/java/com/algorithm/define/LRUCache.java
+++ algorithm/src/main/java/com/algorithm/define/LRUCache.java
@@ -1,4 +1,4 @@
-package com.algorithm;
+package com.algorithm.define;
 
 /**
  * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
@@ -40,7 +40,7 @@ class LRUCache {
     }
 
     public int get(int key) {
-
+        return 0;
     }
 
     public void put(int key, int value) {
